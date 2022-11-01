cba858f89f255d2defe4e22f349eb51241fd32a8
PLARRALDE91 <pablo.larralde@pedidosya.com>
2022-11-01 11:30:33 -0300

    Adds tests for controllers and code refactor

--- src/main/java/com/inditex/similarproductsbackend/exception/ServiceException.java
+++ src/main/java/com/inditex/similarproductsbackend/exception/ServiceException.java
@@ -4,4 +4,7 @@ public class ServiceException extends BaseException {
     public ServiceException(Throwable t) {
         super(t);
     }
+    public ServiceException(String message) {
+        super(message);
+    }
 }
